a2135c5b0f3d80c59bce5d9eee05bd58b87dd1ff
Serb1922 <saintsaintvovich@gmail.com>
2023-03-06 22:50:54 +0300

    reset

--- ToDoList/js.js
+++ ToDoList/js.js
@@ -13,11 +13,6 @@ function createNewCheck(value) {
     newDiv.append(newCheck, newToDo);
     console.log(newDiv)
     return newDiv;
-
-    // Объявиить перменную и присвоить ей Див
-    // добавить класс контейнер созданному дтиву сохраненому в переменную
 }
-// document.body.appendChild(createNewCheck('learn JS'))
-// document.body.appendChild(createNewCheck('lear'))
-// document.body.appendChild(createNewCheck('le'))
+
 createNewCheck('wake');
--- function/123.js
+++ function/123.js
@@ -1,6 +1,15 @@
-// document.querySelector('#submit').onclick = function () {
-//     alert('Вы нажали на кнопку');
-// }
-function alerted() {
-    alert('You pressed the button')
+function sumOfDifferences(arr) {
+    if (arr.length <= 1) {
+        return 0;
+    } else if (arr.length > 1) {
+        arr.sort((a, b) => b - a);
+        return (arr[0] - arr[1]) + (arr[1] - arr[2]);
+    } else {
+        arr.reverse();
+        return arr[0] - arr[1];
+    }
+}
+for (let array = 0; array < array.length; array++) {
+    const element = [];
+
 }
